5a1eba32a8ac4d56dc6b7d63b5623a8767b332d3
root <root@ip-172-31-43-148.ec2.internal>
2020-03-10 01:22:10 +0000

    update path to train files

--- cpu/run.sh
+++ cpu/run.sh
@@ -9,12 +9,12 @@ cd ~/GBM-perf/cpu/run && \
     echo -n "xgboost " && ${R_CMD} < 2-xgboost.R && \
     echo -n "lightgbm " && ${R_CMD} < 3-lightgbm.R && \
     echo -n "catboost " && ${R_CMD} < 4-catboost.R && \
-  ln -sf /train-1m.csv train.csv && \
+  ln -sf ~/train-1m.csv train.csv && \
   echo "1m:" && \
     echo -n "xgboost " && ${R_CMD} < 2-xgboost.R && \
     echo -n "lightgbm " && ${R_CMD} < 3-lightgbm.R && \
     echo -n "catboost " && ${R_CMD} < 4-catboost.R && \
-  ln -sf /train-10m.csv train.csv && \
+  ln -sf ~/train-10m.csv train.csv && \
   echo "10m:" && \
     echo -n "xgboost " && ${R_CMD} < 2-xgboost.R && \
     echo -n "lightgbm " && ${R_CMD} < 3-lightgbm.R && \
